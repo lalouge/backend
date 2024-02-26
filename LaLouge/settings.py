da9a30161c094c47079bafa28478fbd8e11e7984
lalouge <lalouge952@gmail.com>
2024-02-26 07:38:44 +0100

    Adding sqlite3 database for development

--- LaLouge/settings.py
+++ LaLouge/settings.py
@@ -114,6 +114,10 @@ CHANNEL_LAYERS = {
 # Celery Configuration
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
 
 
 # REST_FRAMEWORK = {
